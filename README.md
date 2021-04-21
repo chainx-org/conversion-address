3cab3566f8d8995448df4c6e24135dbc66153aa2
limiu <845217637@qq.com>
2021-04-21 11:33:59 +0800

    add public key and error

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # conversion-address
-请求方式 ：/api/:address/:type 
+请求不同地址方式 ：/api/:address/:type 
+
+请求Public Key方式 ：/api/:address
+
 address为需要转换的地址，type为转换类型。
--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const keyring = require('@polkadot/keyring')
-const { response } = require('express')
+const util = require('@polkadot/util')
 const express = require('express')
 const app = express()
 const cors = require('cors')
+const { u8aToHex } = require('@polkadot/util')
 const requestLogger = (request, response, next) => {
     console.log('Method:', request.method)
     console.log('Path:  ', request.path)
@@ -15,15 +16,29 @@ app.use(cors())
 app.get('/api/:address/:type',(req,res)=> {
     const address = req.params.address
     const type = +req.params.type
-        Address = keyring.encodeAddress(address,type)
+    try {
+      Address = keyring.encodeAddress(address,type)
         res.json(Address)
+    } catch (error) {
+      res.json("请输入合法的地址")
+    }
+        
 }
 )
+app.get('/api/:address',(req,res)=> {
+    const address = req.params.address
+    try {
+      Address = u8aToHex(keyring.decodeAddress(address))
+      res.json(Address)
+    } catch (error) {
+      res.json("请输入合法的地址")
+    }
+    
+})
 const unknownEndpoint = (request, response) => {
     response.status(404).send({ error: 'error address' })
   }
   app.use(unknownEndpoint)
-
 const PORT = 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
